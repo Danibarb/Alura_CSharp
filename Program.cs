2b5eae6173a845152a0fb02cf882058a52ae6c95
Danielle <danibarb@gmail.com>
2022-03-21 21:44:35 -0300

    Add files via upload
    Laços.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+public class Program
+{
+    public static void Main()
+    {
+        for(int contadorLinha = 0; contadorLinha <= 10; contadorLinha++)
+        {
+            for(int contadorColuna = 1; contadorColuna <= contadorLinha; contadorColuna++)
+            {
+                Write("*");
+            }
+            WriteLine();
+        }
+
+        for(char alfabeto ='`'; alfabeto <= 'z'; alfabeto++)
+        {
+            for(char colunasAlfabeto = '`'; colunasAlfabeto < alfabeto; colunasAlfabeto++)
+            {
+                Write(alfabeto); 
+            }
+            WriteLine();
+        }
+    }
+}
