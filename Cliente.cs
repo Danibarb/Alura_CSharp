49cbd00fe9daac1f7083fb33b1e1245f342b25d0
Danielle <danibarb@gmail.com>
2022-03-22 23:01:26 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,9 @@
+namespace POO1
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+    }
+}
---
+++ ContaConstrutores.cs
@@ -0,0 +1,21 @@
+namespace POO1
+{
+    public class ContaConstrutores
+    {
+        public Cliente Titular3 { get; set; }
+        public int Agencia { get; set; }
+        public int Conta { get; set; }
+        public double saldo { get; set; }
+        public static int ObterNumeroContas { get; private set; }
+
+        public ContaConstrutores(int agencia, int conta)
+        {
+            Agencia = agencia;
+            Conta = conta;
+            ObterNumeroContas++;
+            
+        }
+
+
+    }
+}
