d5810ef027a4bcd4ff91ee1f06446e8f72fdb30b
Danielle <danibarb@gmail.com>
2022-03-31 22:03:53 -0300

    Add files via upload
    Usando o atributo nameof(nome do parametro) para explicitar quando Agencia ou Conta estiver em desacordo.

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -4,13 +4,35 @@ namespace POO1
 {
     public class ContaCorrente
     {
-        public string titular;
-        public int agencia;
-        public int conta;
+        public Cliente? titular;
+
+        //Usando apenas GET a propriedade se torna apenas de leitura, podendo apenas ser alterada pelo construtor
+        public int Agencia { get; }
+        public int Conta { get; }
         public double saldo = 100;
 
+        public ContaCorrente(int agencia, int conta)
+        {
+            if(conta <= 0)
+            {
+                throw new ArgumentException("O numero da Conta precisa ser maior que zero", nameof(Conta));
+            }
+            if(agencia <= 0)
+            {
+                throw new ArgumentException("O número da Agência precisa ser maior que zero", nameof(Agencia));
+            }
+            
+            Agencia = agencia;
+            Conta = conta;
+
+           // taxaOperacao = 30 / TotalDeContasCRiadas;
+
+        }
         public double Sacar(double valor)
         {
+            //Propriedade readonly
+            //Agencia = 234;
+
             if(this.saldo < valor)
             {
                 return valor;
